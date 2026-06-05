dd952cfb9ef60d02925793a46acbf0e57f97a2ec
Md Nouman <noumantannu06@gmail.com>
2026-06-05 20:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	if (!input_digit || input_digit.length === 0) return [];
+
+  const map = {
+    "0": "0",
+    "1": "1",
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+  };
+
+  const result = [];
+
+  function backtrack(index, current) {
+    if (index === input_digit.length) {
+      result.push(current);
+      return;
+    }
+
+    const letters = map[input_digit[index]] || "";
+    for (const ch of letters) {
+      backtrack(index + 1, current + ch);
+    }
+  }
+
+  backtrack(0, "");
+  return result;
 }
 
 module.exports = letterCombinations;
